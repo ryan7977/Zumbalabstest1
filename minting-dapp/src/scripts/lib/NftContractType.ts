b6b5fe05e30d07ab57ff2c661f29d7837cf3924c
ryan7977 <102856968+ryan7977@users.noreply.github.com>
2022-04-06 13:48:37 -0700

    Add files via upload

---
+++ minting-dapp/src/scripts/lib/NftContractType.ts
@@ -0,0 +1,7 @@
+// The name below ("Zumbalabstest7") should match the name of your Solidity contract.
+// It can be updated using the following command:
+// yarn rename-contract NEW_CONTRACT_NAME
+// Please DO NOT change it manually!
+import { Zumbalabstest7 as NftContractType } from '../../../../smart-contract/typechain/index';
+
+export default NftContractType;
